9314bf375507a601d5c350f85c2c9965eded2c99
Dan Allen <dan.j.allen@gmail.com>
2017-01-07 22:26:53 -0700

    update lab kit
    - rename SmokeTest to ApparatusTest
    - add client test for PingServlet
    - indent arquillian.xml using 4 spaces
    - update comments in arquillian.xml
    - devide setup and usage sections in README
    - add additional tools section to README
    - adjust README display properties
    - remove Gradle ignores

--- src/main/java/com/tomitribe/training/setup/PingServlet.java
+++ src/main/java/com/tomitribe/training/setup/PingServlet.java
@@ -7,8 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public @WebServlet("/ping") class PingServlet extends HttpServlet {
-     @Override protected void doGet(HttpServletRequest req, HttpServletResponse rsp)
+@WebServlet(name = "Ping", value = "/ping")
+public class PingServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse rsp)
             throws ServletException, IOException {
         rsp.setStatus(200);
         rsp.setContentType("text/plain");
---
+++ src/test/java/com/tomitribe/training/setup/ApparatusTest.java
@@ -0,0 +1,47 @@
+package com.tomitribe.training.setup;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.logging.Logger;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class ApparatusTest {
+    @Deployment
+    public static Archive<?> createTestArchive() {
+        return ShrinkWrap.create(WebArchive.class)
+                .addClasses(PingServlet.class);
+    }
+
+    @Test
+    public void shouldSayHiFromContainer() throws Exception {
+        Logger.getLogger(getClass().getName()).info(
+                "Hello from test running in " + Thread.currentThread().getContextClassLoader());
+    }
+
+    @RunAsClient
+    @Test
+    public void shouldRespondToPing(@ArquillianResource URL webappUrl) throws Exception {
+        URL pingUrl = new URL(webappUrl + "ping");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(pingUrl.openStream()))) {
+            assertThat(reader.readLine(), is(equalTo("here")));
+        }
+        catch (FileNotFoundException e) {
+            fail("Server returned a 404 response for the following path: /ping");
+        }
+    }
+}
